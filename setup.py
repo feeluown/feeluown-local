6942377d0f1cbfe99a336c9168988730608bbf66
cosven <yinshaowen241@gmail.com>
2019-02-01 22:22:23 +0800

    init

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name='fuo_local',
+    version='0.1.dev0',
+    description='feeluown local plugin',
+    author='Cosven',
+    author_email='yinshaowen241@gmail.com',
+    packages=[
+        'fuo_local',
+    ],
+    package_data={
+        '': []
+        },
+    url='https://github.com/feeluown/feeluown-local',
+    keywords=['feeluown', 'plugin', 'local'],
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        ),
+    install_requires=[
+        'feeluown',
+    ],
+    entry_points={
+        'fuo.plugins_v1': [
+            'local = fuo_local',
+        ]
+    },
+)
