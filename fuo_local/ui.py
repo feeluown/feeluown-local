1acc1e06d36ba2051ca19819514ee22b34b1e7b2
cosven <yinshaowen241@gmail.com>
2021-01-26 22:28:55 +0800

    adapt to fuo v3.7

--- fuo_local/ui.py
+++ fuo_local/ui.py
@@ -1,4 +1,5 @@
-from feeluown.containers.table import Renderer
+from feeluown.utils.reader import wrap
+from feeluown.gui.page_containers.table import Renderer
 
 
 class LibraryRenderer(Renderer):
@@ -13,14 +14,13 @@ class LibraryRenderer(Renderer):
         self.tabbar.library_mode()
 
         # render songs
-        self.show_songs(songs_g=None, songs=self.songs, show_count=True)
+        self.show_songs(reader=wrap(self.songs), show_count=True)
 
         # bind signals
         self.toolbar.filter_albums_needed.connect(
             lambda types: self.albums_table.model().filter_by_types(types))
         self.tabbar.show_songs_needed.connect(
-            lambda: self.show_songs(songs_g=None,
-                                    songs=self.songs,
+            lambda: self.show_songs(reader=wrap(self.songs),
                                     show_count=True))
         self.tabbar.show_albums_needed.connect(lambda: self.show_albums(self.albums))
         self.tabbar.show_artists_needed.connect(lambda: self.show_artists(self.artists))
